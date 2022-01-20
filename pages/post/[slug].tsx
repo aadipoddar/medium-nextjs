7bde5006d12a4c2cbe64032813958c42a4668c4e
Aadi Poddar <aadipoddarmail@gmail.com>
2022-01-20 20:23:37 +0530

    Designed the Top Part of our Posts Page

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -8,10 +8,34 @@ interface Props {
 }
 
 function Post({ post }: Props) {
-  console.log(post);
-  
   return <main>
     <Header />
+
+    <img
+      className="h-40 w-full object-cover"
+      src={urlFor(post.mainImage).url()!}
+      alt=""
+    />
+
+    <article className="max-w-3xl mx-auto p-5">
+      <h1 className="text-3xl mt-10 mb-3">{post.title}</h1>
+      <h2 className="text-xl font-light text-gray-500 mb-2">{post.description}</h2>
+
+      <div className="flex items-center space-x-2">
+        <img
+          className="h-10 w-10 rounded-full"
+          src={urlFor(post.author.image).url()!}
+          alt=""
+        />
+
+        <p className="font-extralight text-sm">
+          Blog Post by <span className="text-green-600">{post.author.name}</span> -
+          Published at {new Date(post._createdAt).toLocaleString()}
+        </p>
+      </div>
+
+
+    </article>
   </main>;
 }
 
