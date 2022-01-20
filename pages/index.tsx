571820d44d4ce242767558fb3300310e5b8fa9c5
Aadi Poddar <aadipoddarmail@gmail.com>
2022-01-20 09:56:21 +0530

    Updated Posts and Included Server side rendering

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,13 @@
 import Head from 'next/head'
 import Header from '../components/Header'
+import { sanityClient, urlFor } from '../sanity'
+import { Post } from '../typings';
 
-export default function Home() {
+interface Props {
+  posts: [Post];
+}
+
+export default function Home({ posts }: Props) {
   return (
     <div className="max-w-7xl mx-auto">
       <Head>
@@ -31,3 +37,25 @@ export default function Home() {
     </div>
   )
 }
+
+export const getServerSideProps = async () => {
+  const query = `*[_type == "post"]{
+    _id,
+    title,
+    author -> {
+      name,
+      image
+    },
+    description,
+    mainImage,
+    slug
+  }`;
+
+  const posts = await sanityClient.fetch(query);
+
+  return {
+    props: {
+      posts,
+    },
+  };
+};
